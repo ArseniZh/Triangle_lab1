9ebc00b455bc24e9f1511905ccd030f77d70708d
ArseniZh <szhirnevich@mail.ru>
2022-11-07 01:17:32 +0300

    add toString method in Triangle class

--- src/Triangle.java
+++ src/Triangle.java
@@ -10,33 +10,34 @@ public class Triangle{
             dots.add(new Dot(1.2 + i, 4.3 + i));
         }
     }
-
     public void print () {
         int i = 1;
         for (Dot dot : dots) {
             System.out.println("Dot " + i++ + ":" +
-                    "\n\txCord: " + dot.getxCord() +
-                    "\n\tyCord: " + dot.getyCord());
+                    "\n\txCord: " + dot.getXCord() +
+                    "\n\tyCord: " + dot.getYCord());
         }
     }
-
     public double calculatePerimetr () {
         double perimetr = 0;
         perimetr += calculateSide(dots.get(0), dots.get(1)) + calculateSide(dots.get(0), dots.get(2)) + calculateSide(dots.get(1), dots.get(2));
         return perimetr;
     }
-
     public double calculateSide (Dot dot0, Dot dot1) {
         double side = 0;
-        side = Math.sqrt((dot0.getxCord() - dot1.getxCord()) * (dot0.getxCord() - dot1.getxCord()) +
-                (dot0.getyCord() - dot1.getyCord()) * (dot0.getyCord() - dot1.getyCord()));
+        side = Math.sqrt((dot0.getXCord() - dot1.getXCord()) * (dot0.getXCord() - dot1.getXCord()) +
+                (dot0.getYCord() - dot1.getYCord()) * (dot0.getYCord() - dot1.getYCord()));
         return side;
     }
-
     public double calculateArea () {
         double area = 0;
-        area = 0.5 * Math.abs((dots.get(1).getxCord() - dots.get(0).getxCord()) * (dots.get(2).getyCord() - dots.get(0).getyCord()) -
-                (dots.get(2).getxCord() - dots.get(0).getxCord()) * (dots.get(1).getyCord() - dots.get(0).getyCord()));
+        area = 0.5 * Math.abs((dots.get(1).getXCord() - dots.get(0).getXCord()) * (dots.get(2).getYCord() - dots.get(0).getYCord()) -
+                (dots.get(2).getXCord() - dots.get(0).getXCord()) * (dots.get(1).getYCord() - dots.get(0).getYCord()));
         return area;
+    }
+    public String toString() {
+        return "First dot: " + "\n\txCord: " + dots.get(0).getXCord() + "\n\tyCord: " + dots.get(0).getYCord() +
+                "\nSecond dot: " + "\n\txCord: " + dots.get(1).getXCord() + "\n\tyCord: " + dots.get(1).getYCord() +
+                "\nThird dot: " + "\n\txCord: " + dots.get(2).getXCord() + "\n\tyCord: " + dots.get(2).getYCord();
     }
 }
