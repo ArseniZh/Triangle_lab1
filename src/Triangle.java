498d23e1752400854b89d90d16e16aae50974d5a
ArseniZh <szhirnevich@mail.ru>
2022-11-06 23:07:01 +0300

    add method calulateArea to Triangle class

--- src/Triangle.java
+++ src/Triangle.java
@@ -5,7 +5,7 @@ public class Triangle{
     public int dotNumbers = 3;
     public List<Dot> dots = new ArrayList<>();
 
-    public Triangle() {
+    public Triangle () {
         for (int i = 0; i < dotNumbers; i++) {
             dots.add(new Dot(1.2 + i, 4.3 + i));
         }
@@ -23,7 +23,6 @@ public class Triangle{
     public double calculatePerimetr () {
         double perimetr = 0;
         perimetr += calculateSide(dots.get(0), dots.get(1)) + calculateSide(dots.get(0), dots.get(2)) + calculateSide(dots.get(1), dots.get(2));
-        System.out.println(perimetr);
         return perimetr;
     }
 
@@ -34,4 +33,10 @@ public class Triangle{
         return side;
     }
 
+    public double calculateArea () {
+        double area = 0;
+        area = 0.5 * Math.abs((dots.get(1).getxCord() - dots.get(0).getxCord()) * (dots.get(2).getyCord() - dots.get(0).getyCord()) -
+                (dots.get(2).getxCord() - dots.get(0).getxCord()) * (dots.get(1).getyCord() - dots.get(0).getyCord()));
+        return area;
+    }
 }
